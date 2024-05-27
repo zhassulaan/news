b9cfe42bd279236a689e14f2ef7d41f2bb189612
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-05-27 13:51:30 +0500

    layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class='default-layout'>
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue';
+</script>
---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class='home-layout'>
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import Header from '@/components/Header.vue';
+</script>
