34a83033cac786eb8e0e2f7060cd3c5eab66a6e2
Zhassulan Serikuly <zhass@MacBook-Air-Zhassulan.local>
2024-05-27 15:06:37 +0500

    api

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
   <RouterView />
 </template>
+
+<script>
+export default {
+  name: 'Home',
+  beforeCreate() {
+    this.$store.dispatch('fetchNews');
+  }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/styles/global.css';
+import store from './store';
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
